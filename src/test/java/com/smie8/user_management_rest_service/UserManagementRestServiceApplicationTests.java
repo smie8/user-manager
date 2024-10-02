e33a05766543650d6f3a040419f912661b4c71dd
smie8 <sami.eriksson@yahoo.fi>
2024-10-02 21:46:48 +0300

    Create Spring Boot project using Spring Initializr.

---
+++ src/test/java/com/smie8/user_management_rest_service/UserManagementRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smie8.user_management_rest_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
