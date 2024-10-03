cdde74405eac63f9c6363f0c2ccb3dbce2c8f243
smie8 <sami.eriksson@yahoo.fi>
2024-10-03 08:03:44 +0300

    Create simple summary of each API operation.

--- src/main/java/com/smie8/usermanager/controller/UserController.java
+++ src/main/java/com/smie8/usermanager/controller/UserController.java
@@ -2,6 +2,7 @@ package com.smie8.usermanager.controller;
 
 import com.smie8.usermanager.model.User;
 import com.smie8.usermanager.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -28,29 +29,34 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Get all users")
     @GetMapping(BASE_URL)
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
+    @Operation(summary = "Get user by id")
     @GetMapping(BASE_URL + "/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
         Optional<User> user = userService.getUserById(id);
         return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Create user with a name using request body")
     @PostMapping(BASE_URL)
     public ResponseEntity<User> createUser(@RequestBody User user) {
         User createdUser = userService.createUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     }
 
+    @Operation(summary = "Add user to group")
     @PostMapping(BASE_URL + "/{userId}/groups/{groupId}")
     public ResponseEntity<Void> addUserToGroup(@PathVariable Long userId, @PathVariable Long groupId) {
         userService.addUserToGroup(userId, groupId);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Update user by using request body")
     @PutMapping(BASE_URL + "/{id}")
     public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
         if (!id.equals(user.getId())) {
@@ -61,12 +67,14 @@ public class UserController {
         return ResponseEntity.ok(updatedUser);
     }
 
+    @Operation(summary = "Delete user by id")
     @DeleteMapping(BASE_URL + "/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Remove user from group")
     @DeleteMapping(BASE_URL + "/{userId}/groups/{groupId}")
     public ResponseEntity<Void> removeUserFromGroup(@PathVariable Long userId, @PathVariable Long groupId) {
         userService.removeUserFromGroup(userId, groupId);
--- src/main/java/com/smie8/usermanager/controller/UserGroupController.java
+++ src/main/java/com/smie8/usermanager/controller/UserGroupController.java
@@ -2,6 +2,7 @@ package com.smie8.usermanager.controller;
 
 import com.smie8.usermanager.model.UserGroup;
 import com.smie8.usermanager.service.UserGroupService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -27,47 +28,55 @@ public class UserGroupController {
         this.userGroupService = userGroupService;
     }
 
+    @Operation(summary = "Get all user groups")
     @GetMapping(BASE_URL)
     public List<UserGroup> getAllUserGroups() {
         return userGroupService.getAllUserGroups();
     }
 
+    @Operation(summary = "Get user group by id")
     @GetMapping(BASE_URL + "/{id}")
     public ResponseEntity<UserGroup> getUserGroupById(@PathVariable Long id) {
         Optional<UserGroup> userGroup = userGroupService.getUserGroupById(id);
         return userGroup.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Create user group with a name using request body")
     @PostMapping(BASE_URL)
     public ResponseEntity<UserGroup> createUserGroup(@RequestBody UserGroup userGroup) {
         UserGroup createdUserGroup = userGroupService.createUserGroup(userGroup);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdUserGroup);
     }
 
+    @Operation(summary = "Delete user group by id")
     @DeleteMapping(BASE_URL + "/{id}")
     public ResponseEntity<Void> deleteUserGroup(@PathVariable Long id) {
         userGroupService.deleteUserGroup(id);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Add user to group")
     @PostMapping(BASE_URL + "/{groupId}/users/{userId}")
     public ResponseEntity<Void> addUserToGroup(@PathVariable Long userId, @PathVariable Long groupId) {
         userGroupService.addUserToGroup(userId, groupId);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Remove user from group")
     @DeleteMapping(BASE_URL + "/{groupId}/users/{userId}")
     public ResponseEntity<Void> removeUserFromGroup(@PathVariable Long userId, @PathVariable Long groupId) {
         userGroupService.removeUserFromGroup(userId, groupId);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Remove all users from group")
     @DeleteMapping(BASE_URL + "/{groupId}/users")
     public ResponseEntity<Void> removeAllUsersFromGroup(@PathVariable Long groupId) {
         userGroupService.removeAllUsersFromGroup(groupId);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Remove user from all groups")
     @DeleteMapping(BASE_URL + "/users/{userId}")
     public ResponseEntity<Void> removeUserFromAllGroups(@PathVariable Long userId) {
         userGroupService.removeUserFromAllGroups(userId);
